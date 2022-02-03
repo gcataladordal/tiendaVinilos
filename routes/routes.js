2ba01a4261b08b9defe3f191952b225345da291c
parvovirus <jorge.sanchezcuesta@hotmail.com>
2022-02-03 16:50:08 +0100

    Realizado las validaciones registro

--- routes/routes.js
+++ routes/routes.js
@@ -23,9 +23,10 @@ router.get("/",pages.home);
 // // Viene del formulario para ver el historial
 // router.post("/historial", pages.historial);
 // // Viene de un botón al login
-router.get("/login", pages.login)
+// router.get("/login", pages.login)
 // // Viene de un botón al register
-router.get("/register", pages.register)
+router.get("/registerLogin", pages.register);
+router.post("/registrar", pages.registro);
 // // Viene de un botón de perfil
 // router.get("/perfil", pages.perfil);
 // // Viene de un botón del producto y te lleva a la info de ese producto
